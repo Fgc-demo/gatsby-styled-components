b7781d4a28f72688420b07e40bb70c12a0f4da44
xuanhanh.dev <xuanhanh.setdy@gmail.com>
2023-05-24 15:25:37 +0700

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,26 @@ module.exports = {
     DEV_SSR: true,
   },
   plugins: [
-    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: false,
+        disableVendorPrefixes: true,
+        minify: true,
+        transpileTemplateLiterals: false,
+        ssr: true,
+        pure: true,
+        meaninglessFileNames: ["index", "styles"],
+        fileName: false,
+      },
+    },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: `UA-93349937-2`,
       },
     },
+    `gatsby-plugin-glamor`,
     `gatsby-plugin-offline`,
   ],
 };
